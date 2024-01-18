fe1c9b274c7527895e81fbcffc562031e2c0e245
zeli.lwb <zeli.lwb@oceanbase.com>
2024-01-18 20:41:06 +0800

    [Feat] ycsb stat

--- ycsb-rs/src/main.rs
+++ ycsb-rs/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
 use std::{
     cell::RefCell,
     fs,
@@ -12,6 +14,7 @@ use obkv::dump_metrics;
 use properties::Properties;
 use rand::{rngs::SmallRng, SeedableRng};
 use structopt::StructOpt;
+use tokio::time as tokioTime;
 use workload::CoreWorkload;
 
 use crate::{
@@ -53,9 +56,15 @@ fn run(wl: Arc<CoreWorkload>, db: Rc<dyn DB>, rng: Rc<RefCell<SmallRng>>, operat
     }
 }
 
-async fn load_ob(wl: Arc<CoreWorkload>, db: Arc<OBKVClient>, operation_count: usize) {
+async fn load_ob(
+    wl: Arc<CoreWorkload>,
+    db: Arc<OBKVClient>,
+    operation_count: usize,
+    counter: Arc<AtomicUsize>,
+) {
     for _ in 0..operation_count {
         wl.ob_insert(db.clone()).await;
+        counter.fetch_add(1, Ordering::SeqCst);
     }
 }
 
@@ -64,9 +73,11 @@ async fn run_ob(
     db: Arc<OBKVClient>,
     rng: Arc<Mutex<SmallRng>>,
     operation_count: usize,
+    counter: Arc<AtomicUsize>,
 ) {
     for _ in 0..operation_count {
         wl.ob_transaction(rng.clone(), db.clone()).await;
+        counter.fetch_add(1, Ordering::SeqCst);
     }
 }
 
@@ -88,12 +99,22 @@ fn main() -> Result<()> {
     }
 
     let database = opt.database.clone();
+    let total_ops_count = props.operation_count;
     let thread_operation_count = props.operation_count as usize / opt.threads;
     let actual_client_count = opt.threads / props.obkv_client_reuse;
+
+    // verify count of operations
+    assert_eq!(
+        props.operation_count,
+        (actual_client_count * thread_operation_count * props.obkv_client_reuse) as u64,
+        " 'operationcount' should be an exact multiple of the product of 'obkv_client_reuse' and 'threads'"
+    );
+
     for cmd in opt.commands {
         let start = Instant::now();
         let mut tasks = vec![];
         let mut threads = vec![];
+        let mut db_counters = vec![];
         println!(
             "Database: {database}, Command: {cmd}, Counts Per Threads: {thread_operation_count}"
         );
@@ -106,22 +127,66 @@ fn main() -> Result<()> {
             for _client_idx in 0..actual_client_count {
                 let database = database.clone();
                 let db = db::create_ob(&database, config.clone()).unwrap();
+                // count the ops per client
+                let counter = Arc::new(AtomicUsize::new(0));
+                db_counters.push(counter.clone());
                 for _ in 0..props.obkv_client_reuse {
                     let db = db.clone();
                     let wl = wl.clone();
                     let cmd = cmd.clone();
                     let runtime = runtimes.default_runtime.clone();
+                    let counter_clone = Arc::clone(&counter);
                     tasks.push(runtime.spawn(async move {
                         let rng = Arc::new(Mutex::new(SmallRng::from_entropy()));
                         db.init().unwrap();
                         match &cmd[..] {
-                            "load" => load_ob(wl.clone(), db, thread_operation_count).await,
-                            "run" => run_ob(wl.clone(), db, rng, thread_operation_count).await,
+                            "load" => {
+                                load_ob(wl.clone(), db, thread_operation_count, counter_clone).await
+                            }
+                            "run" => {
+                                run_ob(wl.clone(), db, rng, thread_operation_count, counter_clone)
+                                    .await
+                            }
                             cmd => panic!("invalid command: {cmd}"),
                         };
                     }));
                 }
             }
+            // show progress
+            let stat_duration_sec = props.show_progress_duration;
+            tasks.push(runtimes.default_runtime.spawn(async move {
+                let mut interval = tokioTime::interval(Duration::from_secs(stat_duration_sec));
+                let mut prev_count = 0;
+                loop {
+                    interval.tick().await;
+                    let completed_operations: usize = db_counters
+                        .iter()
+                        .map(|arc| arc.load(Ordering::SeqCst))
+                        .sum();
+                    let ops_per_second =
+                        (completed_operations - prev_count) as f64 / stat_duration_sec as f64;
+                    prev_count = completed_operations;
+                    // estimate remaining time
+                    let remaining_operations = total_ops_count - completed_operations as u64;
+                    let estimated_remaining_time = if ops_per_second > 0.0 {
+                        Duration::from_secs_f64(remaining_operations as f64 / ops_per_second)
+                    } else {
+                        Duration::from_secs(0)
+                    };
+                    println!("\n-------------------------------");
+                    println!(
+                        "Throughput(ops/sec) in previous period: {:.2}",
+                        ops_per_second
+                    );
+                    println!("Runtime: {:?}", start.elapsed());
+                    println!("Estimate remaining time: {:?}", estimated_remaining_time);
+
+                    if completed_operations >= total_ops_count as usize {
+                        println!("All is done");
+                        break;
+                    }
+                }
+            }));
             runtimes.block_runtime.block_on(async move {
                 for task in tasks {
                     task.await.expect("task failed");
@@ -151,13 +216,17 @@ fn main() -> Result<()> {
             }
         }
         let runtime = start.elapsed().as_millis();
+        println!("****************************");
         println!("[OVERALL], ThreadCount, {}", opt.threads);
         println!("[OVERALL], RunTime(ms), {runtime}");
         let throughput = props.operation_count as f64 / (runtime as f64 / 1000.0);
         println!("[OVERALL], Throughput(ops/sec), {throughput}\n");
+        println!("****************************");
     }
 
-    println!("{}", dump_metrics().expect("dump metrics failed"));
+    if props.show_prometheus {
+        println!("{}", dump_metrics().expect("dump metrics failed"));
+    }
 
     Ok(())
 }
